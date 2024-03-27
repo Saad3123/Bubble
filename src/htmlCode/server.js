5a747ff4f0aaf390033269ea041ff25ff700f90a
Saad Hussain Syed <90135919+Saad3123@users.noreply.github.com>
2024-03-27 18:12:27 +0000

    created initial servers and installed packages

---
+++ src/htmlCode/server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, 'index.html');
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        }
+    });
+});
+
+const port = 8888; // Choose a port number
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
